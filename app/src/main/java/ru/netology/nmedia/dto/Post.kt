c0363cb8d22a0658056cea5851833b492a7972f0
IvanDurnev1984 <comp-1984@yandex.ru>
2022-06-14 22:10:20 +0400

    2.2. Architecture: MVVM

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    val likesCount: Int,
+    val repostsCount: Int,
+    val viewsCount: Int,
+    val likedByMe: Boolean = false
+)
---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -0,0 +1,39 @@
+package ru.netology.nmedia.repository
+
+import android.util.MutableDouble
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.dto.Post
+
+class PostRepositoryInMemoryImpl : PostRepository {
+
+    private var post = Post(
+        id = 1,
+        author = "Нетология. Университет интернет-профессий",
+        published = "21 мая в 18:36",
+        content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растем сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку перемен http://netolo.gy/fyb",
+        likesCount = 999,
+        repostsCount = 1099,
+        viewsCount = 999999,
+        likedByMe = false
+    )
+
+    private val data = MutableLiveData(post)
+
+    override fun get(): LiveData<Post> = data
+
+    override fun like() {
+        post = post.copy(likedByMe = !post.likedByMe,likesCount = if (!post.likedByMe) post.likesCount+1 else post.likesCount-1)
+        data.value = post
+    }
+
+    override fun repost() {
+        post = post.copy(repostsCount = post.repostsCount+1)
+        data.value = post
+    }
+
+    override fun view() {
+        post = post.copy(viewsCount = post.viewsCount+1)
+        data.value = post
+    }
+}
